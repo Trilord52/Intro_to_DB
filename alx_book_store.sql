60829d0a6d2099ecb074c48a993fcbd54f441761
Trilord52 <tinbiteyonas52@gmail.com>
2025-12-14 23:36:31 +0300

    database tasks intro fix 5

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 );
 
 -- Order_Details Table
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
